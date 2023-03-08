f892e7b54666eb938144522a25e3e7208c86f010
Luong Duc Long <longluong864@gmail.com>
2023-03-08 14:40:25 +0700

    Update

--- src/modules/appointment/AppointmentView.tsx
+++ src/modules/appointment/AppointmentView.tsx
@@ -1,24 +1,7 @@
-import React, { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import React from "react";
 
 const AppointmentView = () => {
-  const [params] = useSearchParams();
-  const AppointmentId: any = params.get("id");
-  useEffect(() => {
-    async function fetchData() {
-      if (!AppointmentId) return;
-      const colRef = doc(db, "Topics", AppointmentId);
-      const docData = await getDoc(colRef);
-      if (docData) setData(docData.data());
-    }
-    fetchData();
-  }, [topicId]);
-  console.log("data", data);
-  return (
-    <div>
-      <p className="text-xl">{parse(data?.desc || "")}</p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default AppointmentView;
